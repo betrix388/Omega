ac349804471847abb726dca62e000b2f186e87fd
betrix388 <69787574+betrix388@users.noreply.github.com>
2020-08-17 13:36:57 +0545

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,10 @@
-// Initialize Firebase
-
-
-///Edit Under This only
-
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyB11alk2BHUaMhteg9B8hT1twbxnNVBiyY",
+  authDomain: "hacking-3c6b2.firebaseapp.com",
+  databaseURL: "https://hacking-3c6b2.firebaseio.com",
+  projectId: "hacking-3c6b2",
+  storageBucket: "hacking-3c6b2.appspot.com",
+  messagingSenderId: "384862069857",
+  appId: "1:384862069857:web:a9785011457f793f097620",
+  measurementId: "G-JJM3KSTB5D"
 };
-
-     
- 
-////Do Not Edit Below This
-
-firebase.initializeApp(firebaseConfig);
-var database = firebase.database();
